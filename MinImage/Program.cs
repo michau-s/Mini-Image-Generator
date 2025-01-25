f120dec392dc5966614c6b35f7d3e959c0f7dfb3
Michał Szyc <181160412+michau-s@users.noreply.github.com>
2025-01-25 20:28:47 +0100

    Generating images now fully async

--- MinImage/Program.cs
+++ MinImage/Program.cs
@@ -8,12 +8,25 @@ namespace Frontend
     {
         static async Task Main(string[] args)
         {
-            var generator = new ImageGenerator();
-            var image = await generator.Generate(1024, 1024);
-            var misc = new MiscellaneousCommands();
-            misc.Output(image, 1024, 1024, $"./image1.jpeg");
-            misc.Help();
-            misc.FreeImage(image);
+
+
+            var tasks = new List<Task>();
+            for (int i = 0; i < 10; i++)
+            {
+                int index = i;
+
+                tasks.Add(Task.Run(async () =>
+                {
+                    var generator = new ImageGenerator();
+                    var misc = new MiscellaneousCommands();
+
+                    var image = await generator.Generate(1024, 1024);
+                    misc.Output(image, 1024, 1024, $"./image{index}.jpeg");
+
+                }));
+            }
+
+            await Task.WhenAll(tasks);
         }
     }
 }
